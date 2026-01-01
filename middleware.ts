96e4da3b1bca2258ca2cb43445be1e1a3a58fbcf
Priyanshu Singh <priyanshusinghdev2003@gmail.com>
2026-01-01 20:22:20 +0530

    Remove middleware for redirecting to analyze page

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,1 @@
-import { NextRequest, NextResponse } from "next/server";
 
-export function middleware(req: NextRequest) {
-  const hasResult = req.cookies.get("hasResult")?.value;
-
-  if (!hasResult) {
-    return NextResponse.redirect(new URL("/analyze", req.url));
-  }
-
-  return NextResponse.next();
-}
-
-export const config = {
-  matcher: ["/result", "/result/:path*"],
-};
