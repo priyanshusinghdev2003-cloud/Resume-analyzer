7dbdf954fd93ddf3ea215741bdbd9daea87685b4
Priyanshu Singh <priyanshusinghdev2003@gmail.com>
2026-01-01 15:35:03 +0000

    updated rate limit response message

--- src/app/api/rate-limit/route.ts
+++ src/app/api/rate-limit/route.ts
@@ -9,7 +9,7 @@ export async function POST(req: NextRequest) {
       return NextResponse.json({ message: "error", limit: 5 });
     }
     if (Number(limit) >= 5) {
-      return NextResponse.json({ message: "error", limit: 5 });
+      return NextResponse.json({ message: "Used All Token", limit: 0 });
     }
 
     return NextResponse.json({ message: "success", limit: 5 - Number(limit) });
